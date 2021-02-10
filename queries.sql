65581b6d8315b77c504706df7fc22e770a490f76
pabloisrael <pabloisrael@users.noreply.github.com>
2021-02-09 23:39:01 -0300

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,31 @@
+----------------------------------------------
+Acceso a lista de reproducción de un profile
+----------------------------------------------
+
+
+----------------------------------------------
+Acceso a material seleccionado de un profile
+----------------------------------------------
+SELECT * FROM materials_profiles WHERE materials_profiles.profile_name = "Chano";
+
+
+----------------------------------------------
+Acceso a lista con el material popular
+----------------------------------------------
+
+SELECT materials.*, COUNT() AS PlaybackCount 
+FROM materials 
+LEFT JOIN playbacks ON materials.title = playbacks.ref_title
+GROUP BY materials.title
+ORDER BY PlaybackCount DESC;
+
+------------------------------------------------------------------------------------------------------------------------------------------
+Acceso a lista por cada género de preferencia del perfil con el material ordenado descendentemente por grado de afinidad
+------------------------------------------------------------------------------------------------------------------------------------------
+
+SELECT * 
+FROM materials 
+INNER JOIN genres_materials ON genres_materials.material_title = materials.title
+INNER JOIN recommendations ON recommendations.material_title = materials.title AND recommendations.profile_name = "Chano"
+WHERE genres_materials.genre_name = "Accion"
+ORDER BY recommendations.similarity DESC;
