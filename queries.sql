f1a7e724a9b6827b454f5a27d6cd9244cae53471
pabloisrael <pabloisrael@users.noreply.github.com>
2021-02-24 00:02:52 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,16 +1,23 @@
 ----------------------------------------------
-Acceso a lista de reproducción de un profile
+#Acceso a lista de reproducción de un profile
 ----------------------------------------------
 
+Select DISTINCT playbacks.ref_material_title, IF (playbacks.minutes / chapters.duration > 0.90, 1, 0) as FINISHED from chapters 
+INNER JOIN playbacks ON (chapters.title = playbacks.ref_title AND playbacks.ref_type = 'chapters'  )  
+WHERE playbacks.minutes / chapters.duration <= 0.90 AND playbacks.profile_name = 'luciano'
+UNION
+Select playbacks.ref_material_title, IF (playbacks.minutes / movies.duration > 0.90, 1, 0) as FINISHED  from movies 
+INNER JOIN playbacks ON (movies.material_title = playbacks.ref_title AND playbacks.ref_type = 'movies')  
+WHERE playbacks.minutes / movies.duration <= 0.90;
 
 ----------------------------------------------
-Acceso a material seleccionado de un profile
+#Acceso a material seleccionado de un profile
 ----------------------------------------------
 SELECT * FROM materials_profiles WHERE materials_profiles.profile_name = "luciano";
 
 
 ----------------------------------------------
-Acceso a lista con el material popular
+#Acceso a lista con el material popular
 ----------------------------------------------
 
 SELECT materials.*, COUNT() AS PlaybackCount 
@@ -20,7 +27,7 @@ GROUP BY materials.title
 ORDER BY PlaybackCount DESC;
 
 ------------------------------------------------------------------------------------------------------------------------------------------
-Acceso a lista por cada género de preferencia del perfil con el material ordenado descendentemente por grado de afinidad
+#Acceso a lista por cada género de preferencia del perfil con el material ordenado descendentemente por grado de afinidad
 ------------------------------------------------------------------------------------------------------------------------------------------
 
 SELECT * 
