d8976464b733a3dcd3e8b3a9711900ff624489a5
pabloisrael <pabloisrael@users.noreply.github.com>
2021-03-01 19:20:47 -0300

    Update db-creation-v2.sql

--- db-creation-v2.sql
+++ db-creation-v2.sql
@@ -1,16 +1,3 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema netflix
--- -----------------------------------------------------
-
 -- -----------------------------------------------------
 -- Schema netflix
 -- -----------------------------------------------------
@@ -540,8 +527,3 @@ CREATE TABLE IF NOT EXISTS `netflix`.`subscriptions` (
     REFERENCES `netflix`.`plans` (`name`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
